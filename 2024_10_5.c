9484c7db0b1bbf7d798b34ed0f469b1d2dcde201
安中 蓮 <1584733077@qq.com>
2024-10-05 20:38:41 +0800

    2024_10_5

---
+++ 2024_10_5.c
@@ -0,0 +1,68 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+////学生
+//struct Stu
+//{
+//	//以下为结构体的成员
+//	char name[20];
+//	int age;
+//	char sex[10];
+//	char tele[12];
+//};
+//
+//void print(struct Stu* ps)
+//{
+//	printf("%s %d %s %s\n", (*ps).name, (*ps).age, (*ps).sex, (*ps).tele);
+//	printf("%s %d %s %s\n", ps->name, ps->age, ps->sex, ps->tele);
+//	//->
+//	//结构体指针变量->成员名
+//}
+//int main(void)
+//{
+//	struct Stu  s = {"张三", 20, "男", "15938742734"};
+//
+//	//结构体对象  .成员名
+//	//printf("%s %d %s %s\n", s.name, s.age, s.sex, s.tele);
+//	print(&s);
+//	return 0;
+//}
+//int main(void)
+//{
+	/*if (3 != 5)
+		printf("hehe\n");*/
+	/*int age = 20;
+
+	if (age > 18)
+	{
+		printf("成年\n");
+	}
+		
+	else
+	{
+		printf("未成年\n不能饮酒\n"); 
+	}*/
+	/*int age = 10;
+	scanf("%d", &age);
+	if (age < 18)
+		printf("青少年\n");
+	else if (age >= 18 && age < 28)
+		printf("青年");
+	else if (age >= 28 && age < 40)
+		printf("中年\n");
+	else if (age >= 40 && age < 60)
+		printf("壮年\n");
+	else if (age >= 60 && age <= 100)
+		printf("老年\n");
+	else
+		printf("老寿星\n");
+		
+	return 0;
+}*/
+	
+int main(void)
+{
+
+
+	return 0;
+}
