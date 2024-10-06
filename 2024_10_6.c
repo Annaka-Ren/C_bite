8f0093e235a9b5091a4cbd903ae945764db02982
安中 蓮 <1584733077@qq.com>
2024-10-06 20:15:15 +0800

    2024_10_6

---
+++ 2024_10_6.c
@@ -0,0 +1,121 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+//1.变量的命名（有意义，规范）
+//2.空格，空行，换行
+//int main(void)
+//{
+//	//判断一个数是否为奇数
+//	int num = 0;
+//	scanf("%d", &num);
+//	//判断
+//	if (num % 2 == 1)
+//		printf("奇数\n");
+//	else
+//		printf("NO\n");
+//
+//	return 0;
+//}
+//int main(void)
+//{
+//	int i = 1;
+//	while (i <= 100)
+//	{
+//
+//		if (i % 2 == 1)
+//		{
+//			printf("%d\n", i);
+//		}
+//		i++;
+//	}
+//	return 0;
+//}
+//int main(void)
+//{
+//	int day = 0;
+//	scanf("%d", day);
+//	if (1 == day)
+//	{
+//		printf("星期一\n");
+//	}
+//	else if (2 == day)
+//	{
+//		printf
+//	}
+//	return 0;
+//}
+//int main(void)
+//{
+//	int day = 0;
+//	scanf("%d", &day);
+//	switch (day)
+//	{
+//	case 1:
+//		printf("星期一\n");
+//		break;
+//	case 2:
+//		printf("星期二\n");
+//		break;
+//	case 3:
+//		printf("星期三\n");
+//		break;
+//	case 4:
+//		printf("星期四\n");
+//		break;
+//	case 5:
+//		printf("星期五\n");
+//		break;
+//	case 6:
+//		printf("星期六\n");
+//		break;
+//	case 7:
+//		printf("星期日\n");
+//		break;
+//
+//	}
+//	return 0;
+//}
+//int main(void)
+//{
+//	int day = 0;
+//	scanf("%d", &day);
+//	switch (day)
+//	{
+//	case 1:
+//		
+//	case 2:
+//		
+//	case 3:
+//		
+//	case 4:
+//		
+//	case 5:
+//		printf("weekday\n");
+//		break;
+//	case 6:
+//		printf("星期六\n");
+//		break;
+//	case 7:
+//		printf("星期日\n");
+//		break;
+//	default:
+//		printf("选择错误\n");
+//
+//	}
+//	return 0;
+//}
+//打印1-10数字
+
+//while循环中的
+//break是用于永久的终止循环
+//continue的作用是跳过本次循环后面的代码，直接去判断部分，进行下一次循环的判断
+//int main(void)
+//{
+//	int num = 1;
+//	while (10 >= num)
+//	{ 
+//		printf("%d\n", num);
+//		num++;
+//	}
+//			return 0;
+//}
