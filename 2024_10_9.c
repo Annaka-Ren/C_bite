821d8f9004361c13d9e73b4a6060e4fd7da8028d
安中 蓮 <1584733077@qq.com>
2024-10-09 18:30:57 +0800

    2024_10_9

---
+++ 2024_10_9.c
@@ -0,0 +1,97 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+//int main(void)
+//{
+//	int max, i, num;
+//	i = 1;
+//	scanf("%d", &num);
+//	max = num;
+//	while (i < 3)
+//	{
+//		scanf("%d", &num);
+//		if (num > max)
+//		{
+//			max = num;
+//		}
+//		i++;
+//	}
+//	printf("%d", max);
+//	return 0;
+//}
+//int main(void)
+//{
+//	int 
+//	
+//	printf("%d\n", b = a + b);
+//	printf("%d\n", a = 2 * b);
+//	return 0;
+//}
+
+//for(表达式1;表达式2;表达式3)
+//   循环语句
+//表达式1为初始化部分，用于初始化循环变量
+//表达式2为条件判断部分，用于判断循环时候终止
+//表达式3为调整部分，用于循环条件的调整
+//int main(void)
+//{
+//	int i = 0;
+//	for (i = 1; i <= 10; i++)
+//	{
+//
+//		if (i == 5)
+//			continue;
+//		
+//		printf("%d", i);
+//		i++;
+//	}
+//	return 0;
+//}
+//int main(void)
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	//0~9
+//	int i = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	return 0;
+//}
+//for循环的判断部分省略意味着判断恒成立
+//for (;;)
+//{
+//	printf("hehe\n");
+//}
+//int main(void)
+//{
+//	int i = 0;
+//	int j = 0;
+//	for(i = 0; i < 10; i++)
+//	{
+//		for(j = 0; j < 10; j++)
+//		{
+//			printf("hehe\n");
+//		}
+//	}
+//	return 0;
+//}
+//int main(void)
+//{
+//	int i = 1;
+//	do 
+//	{
+//		if (i == 5)
+//			break;
+//        printf("%d ", i);
+//	    i++;
+//	}
+//		
+//	while (i <= 10);
+//	return 0;
+//}
+int main(void)
+{
+
+	return 0;
+}
