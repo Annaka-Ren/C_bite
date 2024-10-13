84ac8232af981da786cb23b9ae004e7d505e3be7
安中 蓮 <1584733077@qq.com>
2024-10-13 20:51:01 +0800

    2024_10_13

---
+++ 2024_10_13.c
@@ -0,0 +1,167 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//int main(void)
+//
+////输入一个整数，判断是否为奇数
+//{
+//	int num = 0;
+//	//输入
+//	scanf("%d",&num);
+//	//判断
+//	if (num % 2 == 1) //判断相等用==
+//        printf("%d 是奇数\n", num);
+//	else
+//	{
+//		printf("%d 是偶数\n", num);
+//	}
+//
+//	return 0;
+//}
+//int main(void)
+//{
+//	int age = 0;//年龄
+//	scanf("%d", &age);//输入
+//	if (age >= 18)
+//	{
+//		printf("成年\n");
+//	}
+//	else
+//	{
+//		printf("未成年\n");
+//	}
+//	return 0;
+//}
+//int main(void)
+//{
+//
+//	int num = 0;
+//	//输入
+//	printf("请输入一个整数：\n");
+//	scanf("%d", &num);
+//	if (num == 0)
+//	{
+//		printf("num是 0\n");
+//	}
+//	else//正数 or 负数
+//	{
+//		if (num > 0)
+//		{
+//			printf("num是正数\n");
+//		}
+//		else
+//		{
+//			printf("num是负数\n");
+//		}
+//	}
+//	return 0;
+//}
+//更加实用的修改版
+//int main(void)
+//{
+//	int num = 0;
+//	scanf("%d", &num);
+//	if (num == 0)
+//		printf("num是0");
+//	else if (num > 0)
+//		printf("num是正数");
+//	else
+//		printf("num是负数");
+//	return 0;
+//}
+//int main(void)
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d", &a);
+//	b = (a > 5 ? 3: -3);
+//	//if(a>5)
+//	//    b = 3;
+//	//else
+//	//    b = -3
+//	printf("%d", b);
+//	return 0;
+//}
+//条件操作符
+//int main(void)
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d %d", &a, &b);
+//	//求出a和b的最大值
+//	/*if (a > b)
+//		m = a;
+//	else
+//		m = b;*/
+//	m = (a > b ? a : b);
+//	printf("%d\n", m);
+//	return 0;
+//}
+//int main(void)
+//{
+//	int flag = 0;
+//	scanf("%d", &flag);
+//	//flag为真，do something
+//	/*if (flag)
+//		printf("do something\n");*/
+//	if (!flag)
+//		printf("do something\n");
+//	return 0;
+//}
+//int main(void)
+//{
+//	int year = 0;
+//	scanf("%d", &year);
+//	if (year % 4 == 0 && year % 100 != 0)
+//		printf("闰年\n");
+//	else if (year % 400 == 0)
+//		printf("闰年\n");
+//	else
+//		printf("不是闰年\n");
+//	return 0;
+//}
+//int main(void)
+//{
+//	int year = 0;
+//	scanf("%d", &year);
+//	if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+//		printf("闰年\n");
+//	else
+//		printf("不是闰年\n");
+//	return 0;
+//}
+//int main(void)
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	if (n % 3)//0,1,2
+//		printf("余数是零\n");
+//	else if (n % 3 == 1)
+//		printf("余数是1\n");
+//	else
+//		printf("余数是2\n");
+//	return 0;
+//}
+//int main(void)
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	switch (n % 3)//int short long, long long,char
+//	{
+//	case 0:
+//		printf("余数是0\n"); 
+//		break;
+//	case 1:
+//		printf("余数是1\n"); 
+//		break;
+//	case 2:
+//		printf("余数是2\n");
+//  default:
+//      printf("输入错误")
+//	}
+//	return 0;
+//}
+int main(void)
+{
+
+	return 0;
+}
