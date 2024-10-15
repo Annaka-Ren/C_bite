2a1a2a9c52364e0b79f27952587ebd799804903b
安中 蓮 <1584733077@qq.com>
2024-10-15 21:18:45 +0800

    2024_10_15

---
+++ 2024_10_15.c
@@ -0,0 +1,79 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//int main()
+//{
+//    int a, b;
+//    do {
+//        scanf("%d %d", &a, &b);
+//       } while ((a < 0 && a>100) && (b <= 0 && b > 100));
+//    printf("%d + %d = %d\n", a, b, a + b);
+//    printf("%d - %d = %d\n", a, b, a - b);
+//    printf("%d * %d = %d\n", a, b, (a * b));
+//    if ((1.0)*a / b == (a / b))
+//        printf("%d / %d = %.0f\n", a, b, (1.0) * a / b);
+//    else
+//        printf("%d / %d = %.2f\n", a, b, (1.0) * a / b);
+//    
+//    
+//    
+//
+//    return 0;
+//}
+
+//int main(void)
+//{
+//	unsigned int start, end;
+//	start = 0;
+//	end = 0;
+//	unsigned int h, m;
+//	h = 0;
+//	m = 0;
+//	scanf("%d %d", &start, &end);
+//	h = end / 100 - start / 100;
+//	if (end % 100 > start % 100)
+//	{
+//		m = end % 100 - start % 100;
+//	}
+//	else//例如终点16:17，起点15:47
+//	{
+//		m = end % 100 - start % 100 + 60;
+//		h--;
+//	}
+//	
+//	
+//	printf("%02d:%02d", h, m);
+//	return 0;
+//}
+//int main(void)
+//{
+//	int a = 0;
+//	int i = 0;
+//	scanf("%d", &a);
+//	switch (a / 10)
+//	{
+//    
+//	case 9:
+//		printf("A\n"); break;
+//	case 8:
+//		printf("B\n"); break;
+//	case 7:
+//		printf("C\n"); break;
+//	case 6:
+//		printf("D\n"); break;
+//	case 5:
+//	case 4:
+//	case 3:
+//	case 2:
+//	case 1:
+//    case 0:
+//		printf("E\n"); break;
+//	default:
+//		printf("A\n");
+//	}
+//	return 0;
+//}
+int main(void)
+{
+
+	return 0;
+}
